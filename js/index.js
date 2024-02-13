cf9c1ff058eb29c9232be853c1172b966c960726
anfasdx <kpanfas6@gmail.com>
2024-02-13 23:16:50 +0530

    Products contents rendered w/ JavaScript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // Cart
-const cartIcon = document.querySelector('#cart-icon');
+const cartIcon = document.querySelector('.cart-icon');
+// const cartIcon1 = document.querySelector('#cart-icon1');
 const cart = document.querySelector('.cart');
 const closeCart = document.querySelector('#close-cart');
+
 
 function toggleActive(){
   if(cart.classList.contains('active')){
@@ -11,8 +13,10 @@ function toggleActive(){
   }
 };
 
+
 cartIcon.addEventListener('click', toggleActive);
 closeCart.addEventListener('click', toggleActive);
+
 // But old is better 
 
 // // openCart
@@ -25,20 +29,26 @@ closeCart.addEventListener('click', toggleActive);
 // }
 
 // cartWorking JS
+
 if (document.readyState == 'loading'){
     document.addEventListener('DOMContentLoaded', ready);
 }else {
     ready();
 }
+
+// ready();
+
 // makingFunction
 function ready() {
     // Remove Items From Cart
-    var removeCartButtons = document.getElementsByClassName('remove-cart');
-    console.log(removeCartButtons);
-    for (var i = 0; i < removeCartButtons.length;i++) {
-        var button = removeCartButtons[i];
-        button.addEventListener('click', removeCartItem);
-    }
+    // var removeCartButtons = document.getElementsByClassName('remove-cart');
+    var i = 1;
+    console.log(`\n ${i} `+"heet ");
+    // for (var i = 0; i < removeCartButtons.length;i++) {
+    //   console.log('\n '+i);
+    //   var button = removeCartButtons[i];
+    //     button.addEventListener('click', removeCartItem);
+    // }
     // Quantity Changes 
     var quantityInputs = document.getElementsByClassName('cart-quantity');
     for (var i = 0; i < quantityInputs.length;i++){
@@ -55,7 +65,7 @@ function ready() {
     document
       .getElementsByClassName('btn-buy')[0]
       .addEventListener('click', buyButtonClicked);
-}
+};
 
 // Buy Button
 function buyButtonClicked() {
@@ -75,19 +85,23 @@ function buyButtonClicked() {
     const phone = '+919633316673';
     const url = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
-    const cartContent = document.querySelector('.cart-content');
+    const cartContent = document.querySelector('.cart-contents');
     while (cartContent.hasChildNodes()) {
       cartContent.removeChild(cartContent.firstChild);
     }
     updatetotal();
-  }
+  };
    
  // Remove Items From Cart 
- function removeCartItem(event){
+function removeCartItem(event){
     var buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
+    buttonClicked.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+    console.log();
+    // NearCart.getElementsByClassName('cartBox')[0].remove();
+    console.log("Success!...");
     updatetotal();
- }
+};
+
 //  Quantity changes
 function quantityChanged(event){
     var input = event.target;
@@ -95,7 +109,7 @@ function quantityChanged(event){
         input.value = 1;
     }
     updatetotal();
-}
+};
 // Add To Cart
 function addCartClicked(event) {
     var button = event.target;
@@ -105,10 +119,13 @@ function addCartClicked(event) {
     var productImg = shopProducts.getElementsByClassName('product-img')[0].src;
     addProductToCart(title, price, productImg);
     updatetotal();
-}
+};
+
+// carts item  here from 6-8
+
 function addProductToCart(_title, _price, _productImg) {
-    var cartContent = document.querySelector(".cart-contents");
-    var cartItems = cartContent.querySelectorAll(".cart-box");
+  var cartContent = document.querySelector(".cart-contents");
+var cartItems = cartContent.querySelectorAll(".cartBox");
     for (var i = 0; i < cartItems.length; i++) {
       var cartProductTitle = cartItems[i].querySelector(".cart-product-title");
       if (cartProductTitle.innerText === _title) {
@@ -118,9 +135,9 @@ function addProductToCart(_title, _price, _productImg) {
     }
   
     var cartShopBox = document.createElement("div");
-    cartShopBox.classList.add("cart-box");
+    cartShopBox.classList.add("cartBox");
   
-    var cartBoxContent = `<div class="flex flex-col md:flex-row md:gap-4">
+    var cartBoxContent = `<div class="cart-box flex flex-col md:flex-row md:gap-4">
     <div class="w-full bg-white px-5 py-3">
       <div class="flex mt-5 mb-3 space-x-4">
         <div class="w-3/5 flex flex-col">
@@ -128,9 +145,9 @@ function addProductToCart(_title, _price, _productImg) {
         </div>
         <div class="w-4/5 flex flex-col justify-between">
           <div class="w-full flex-col">
-            <h3 class="font-semi text-lg">${_title}</h3>
+            <h3 class="font-semi text-lg cart-product-title">${_title}</h3>
             <p class="text-gray-500 text-sm">Large • XL </p>
-            <p class="text-gray-500 text-sm"><span class="font-semi text-black">${_price}</span></p>
+            <p class="text-gray-500 text-sm"><span class="font-semi text-black cart-price">${_price}</span></p>
             <select class="cart-quantity border rounded-md p-1 pr-4 text-gray-600 focus:outline-none">
               <option value="1">1</option>
               <option value="2">2</option>
@@ -144,15 +161,17 @@ function addProductToCart(_title, _price, _productImg) {
             </p>
           </div>
         </div>
-        <button class="remove-cart hidden md:inline bg-purple-600 hover:bg-purple-700 text-white font-medium text-sm rounded-md p-2 mt-3">
+      <div class="remove-cart">
+        <button class=" hidden md:inline bg-purple-600 hover:bg-purple-700 text-white font-medium text-sm rounded-md p-2 mt-3">
           <span class="hidden md:inline">Remove</span>
         </button>
-        <div class="remove-cart">
+        <div>
           <button class="md:hidden text-white font-medium text-sm rounded-md p-2 mt-2">
             <svg class="w-4 h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
             </svg>
           </button>
+        </div>
         </div>
       </div>
     </div>
@@ -171,7 +190,7 @@ function addProductToCart(_title, _price, _productImg) {
 
     cartShopBox.innerHTML = cartBoxContent;
     cartContent.append(cartShopBox);
-  
+
     cartShopBox
       .querySelector(".remove-cart")
       .addEventListener("click", removeCartItem);
@@ -179,12 +198,13 @@ function addProductToCart(_title, _price, _productImg) {
     cartShopBox
       .querySelector(".cart-quantity")
       .addEventListener("change", quantityChanged);
-  }
+  };
+
 
 //  Update Total 
 function updatetotal() {
-    var cartContent = document.getElementsByClassName('cart-content')[0];
-    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var cartContent = document.getElementsByClassName('cart-contents')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cartBox');
     var total = 0;
     for (var i =0; i < cartBoxes.length; i++) {
         var cartBox = cartBoxes[i];
@@ -197,4 +217,4 @@ function updatetotal() {
         // if price Contain some Cents Value
         total = Math.round(total * 100) / 100;
         document.getElementsByClassName('total-price')[0].innerText = "$" + total;
-}
+};
